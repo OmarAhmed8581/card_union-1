f91a978463e12c2a8d7f31e3f9dd8f202da08383
Omar Ahmed/TDD/KHI <omar.ahmed@itecknologi.internal>
2023-09-08 21:43:13 +0500

    first commit

--- backup(2023-08-09)/CartUnion-Backend-master/CartUnion-Backend-master/src/routes/admin/initialData.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { initialData } = require("../../controller/admin/initialData");
-const { requireSignin, adminMiddleware} =require('../../common-middleware/index');
-const router = express.Router();
-
-router.post("/initialdata", requireSignin, adminMiddleware, initialData);
-
-module.exports = router;
